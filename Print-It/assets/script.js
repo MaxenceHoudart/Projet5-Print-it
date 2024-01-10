2192420448a6c92857b51804e5f0d80b6c97330e
MaxenceHoudart <142988468+MaxenceHoudart@users.noreply.github.com>
2024-01-10 10:37:28 +0100

    Update script.js

--- Print-It/assets/script.js
+++ Print-It/assets/script.js
@@ -25,3 +25,34 @@ const dots = document.querySelectorAll('.dot'); // Sélectionnez tous les points
 
 let currentIndex = 0;
 
+// Fonction pour mettre à jour les points indicateurs
+function updateDots(index) {
+    dots.forEach((dot, i) => {
+        if (i === index) {
+            dot.classList.add('dot_selected'); // Ajoutez la classe pour le point actuel
+        } else {
+            dot.classList.remove('dot_selected'); // Supprimez la classe pour les autres points
+        }
+    });
+}
+
+// Fonction pour mettre à jour les points indicateurs, l'image et le texte
+function updateCarousel(index, direction) {
+      //correction du bug pour la première et la dernière image
+      if (currentIndex === -1 && direction === 'left') {
+        currentIndex = slides.length - 1;
+    } else if (currentIndex === slides.length && direction === 'right') {
+        currentIndex = 0;
+    }
+
+    // Mettre à jour l'image
+    const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+    bannerImg.src = imagePath;
+    bannerImg.alt = `Slide ${currentIndex + 1}`;
+
+    // Mettre à jour le texte
+    const tagLine = slides[currentIndex].tagLine;
+    document.querySelector('p').innerHTML = tagLine;
+
+    console.log(`Clic sur la flèche ${direction}`);
+}
